86b2058be556ed676a975f07c80fa70166e1c1a2
Cuvii <i@cuvii.dev>
2025-06-07 10:39:45 +0800

    chore: update ESLint configuration to include Prettier integration and add related dependencies in package.json and pnpm-lock.yaml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default antfu({
-  stylistic: false,
-})
+export default antfu(
+  {
+    stylistic: false,
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+);
